6aca232bdf9e6d21bdde8a2e2d4edaf768166870
Leo Phan <phanthanhdatftu@gmail.com>
2023-06-14 12:12:44 +0700

    change URL

--- src/components/Layout/Menu/index.js
+++ src/components/Layout/Menu/index.js
@@ -24,63 +24,72 @@ const Header = (props) => {
 	// const { children } = props;
 
 	return (
-		<ul className="menu">
-			<li className="dropdown">
-				<a href="https://thichhoidap.com/about-us">
-					About Us <i class="fa fa-chevron-down fa-xs"></i>
-				</a>
-				<div class="dropdown-content">
-					<a href="https://thichhoidap.com/founders-story">Founder's Story</a>
-					<a href="#mWteam">mW Team & Board of Advisor</a>
-					<a href="#work">Work with Us</a>
-					<a href="#contact">Contact Us</a>
-				</div>
-			</li>
-			<li>
-				<a href="https://thichhoidap.com/mw-roadmap">
-					mW Roadmap
-				</a>
-			</li>
-			<li className="dropdown">
-				<a href="https://thichhoidap.com/invest">
-					Invest  <i class="fa fa-chevron-down fa-xs"></i>
-				</a>
-				<div class="dropdown-content">
-					<a href="#news">mW Portfolios</a>
-					<a href="#news">Structured Products (Launching 2025)</a>
-				</div>
-			</li>
-			<li className="dropdown">
-				<a href="https://thichhoidap.com/debt">
-					Debt  <i class="fa fa-chevron-down fa-xs"></i>
-				</a>
-				<div class="dropdown-content">
-					<a href="#news">Housing Loans</a>
-					<a href="#news">Credit Cards</a>
-				</div>
-			</li>
-			<li className="dropdown">
-				<a className="active dropbtn" href="/">
-					News & Insights <i class="fa fa-chevron-down fa-xs"></i>
-				</a>
-				<div class="dropdown-content">
-					<a href="#news">Learn 101</a>
-					<a href="#insights">Podcasts & Webinars</a>
-					<a href="#podcasts">Artificial Inteligence</a>
-				</div>
-			</li>
-			<li>
-				<a href="https://thichhoidap.com/support">
-					Support
-				</a>
-			</li>
-			{/* <li className="pr-0">
+		<div>
+			<ul className="menu">
+				<li className="dropdown">
+					<a href="https://realwealth.ai/about-us">
+						About Us <i class="fa fa-chevron-down fa-xs"></i>
+					</a>
+					<div class="dropdown-content">
+						<a href="https://realwealth.ai/founders-story">Founder's Story</a>
+						<a href="#mWteam">mW Team & Board of Advisor</a>
+						<a href="#work">Work with Us</a>
+						<a href="#contact">Contact Us</a>
+					</div>
+				</li>
+				<li>
+					<a href="https://realwealth.ai/mw-roadmap">
+						mW Roadmap
+					</a>
+				</li>
+				<li className="dropdown">
+					<a href="https://realwealth.ai/invest">
+						Invest  <i class="fa fa-chevron-down fa-xs"></i>
+					</a>
+					<div className="dropdown-content">
+						<a href="#news">mW Portfolios</a>
+						<a href="#news">Structured Products (Launching 2025)</a>
+					</div>
+				</li>
+				<li className="dropdown">
+					<a href="https://realwealth.ai/debt">
+						Debt  <i class="fa fa-chevron-down fa-xs"></i>
+					</a>
+					<div className="dropdown-content">
+						<a href="#news">Housing Loans</a>
+						<a href="#news">Credit Cards</a>
+					</div>
+				</li>
+				<li className="dropdown">
+					<a className="active dropbtn" href="/">
+						News & Insights <i class="fa fa-chevron-down fa-xs"></i>
+					</a>
+					<ul className="dropdown-content">
+						<li className="dropdown"><a href="/">Learn 101</a>
+							<div className="dropdown-menu">
+								<ul className="dropdown-content">
+									<li><a href="#news">Personal Finance</a></li>
+									<li><a href="#news">Trending Topics</a></li>
+								</ul>
+							</div>
+						</li>
+						<li><a href="#insights">Podcasts & Webinars</a></li>
+						<li><a href="#podcasts">Artificial Inteligence</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="https://realwealth.ai/support">
+						Support
+					</a>
+				</li>
+				{/* <li className="pr-0">
 				<a href="https://app.mywealth.ai" className="custom-button">Login</a>
 			</li> */}
-			<li>
-				<SelectLanguages />
-			</li>
-		</ul>
+				<li>
+					<SelectLanguages />
+				</li>
+			</ul>
+		</div>
 	);
 };
 
